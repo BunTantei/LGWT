e04f39d5d55db91764071000e814e8201dd219f1
文森特 <wstntgg@gmail.com>
2025-06-26 03:03:53 +0200

    yolo

---
+++ helloworld/hello.go
@@ -0,0 +1,33 @@
+// Package helloworld provides functions for greeting people in different languages.
+package helloworld
+
+const (
+	englishHelloPrefix  = "Hello, "
+	spanishHelloPrefix  = "Hola, "
+	spanishLanguage     = "Spanish"
+	frenchHelloPrefix   = "Bonjour, "
+	frenchLanguage      = "French"
+	japaneseLanguage    = "Japanese"
+	japaneseHelloPrefix = "こんにちは, "
+)
+
+func Hello(name, language string) string {
+	if name == "" {
+		name = "World"
+	}
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case spanishLanguage:
+		prefix = spanishHelloPrefix
+	case frenchLanguage:
+		prefix = frenchHelloPrefix
+	case japaneseLanguage:
+		prefix = japaneseHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
+}
