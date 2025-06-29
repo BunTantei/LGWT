c1f0851fb7b82a9b454d83f67300cfbff82819f4
文森特 <wstntgg@gmail.com>
2025-06-29 23:47:55 +0200

    Switching the itter test from normal to table driven one

--- iteration/itter_test.go
+++ iteration/itter_test.go
@@ -3,10 +3,30 @@ package iteration
 import "testing"
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
+	testCases := []struct {
+		name     string
+		a        string
+		expected string
+	}{
+		{
+			name:     "5 a",
+			a:        "a",
+			expected: "aaaaa",
+		},
+		{
+			name:     "5 1",
+			a:        "1",
+			expected: "11111",
+		},
+	}
 
-	if repeated != expected {
-		t.Errorf("expected %q but got %q", repeated, expected)
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			result := Repeat(tc.a)
+
+			if result != tc.expected {
+				t.Errorf("Repeat(%s) = %s; want %s", tc.a, result, tc.expected)
+			}
+		})
 	}
 }
