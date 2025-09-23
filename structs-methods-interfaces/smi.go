c5a93d90b35173ac34a4a2883f831db626169576
bun <vincent.tantei@proton.me>
2025-09-23 05:19:22 +0200

    working on struct

--- structs-methods-interfaces/smi.go
+++ structs-methods-interfaces/smi.go
@@ -1,6 +1,6 @@
-// Package structsmethodsinterfaces to learn about structs, methods and interfaces
+// Package structs-methods-interfaces to learn about structs, methods, and interfaces
 package structsmethodsinterfaces
 
 func Perimeter(width, height float64) float64 {
-	return 0.0
+	return 2 * (width + height)
 }
