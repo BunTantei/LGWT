6cdf2e2db586b7ccd33a5a390ec343a83009e668
bun <vincent.tantei@proton.me>
2025-11-14 09:55:06 +0100

    switching the test area to TDT as well

--- structs-methods-interfaces/smi_test.go
+++ structs-methods-interfaces/smi_test.go
@@ -2,15 +2,6 @@ package structsmethodsinterfaces
 
 import "testing"
 
-func TestArea(t *testing.T) {
-	got := Area(12.0, 6.0)
-	want := 72.0
-
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
-	}
-}
-
 func TestPerimerter(t *testing.T) {
 	testCases := []struct {
 		name     string
@@ -30,12 +21,6 @@ func TestPerimerter(t *testing.T) {
 			b:        0.0,
 			expected: 0.0,
 		},
-		{
-			name:     "calculating a negative value Perimeter",
-			a:        -10.0,
-			b:        -10.0,
-			expected: -40.0,
-		},
 	}
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
@@ -43,6 +28,37 @@ func TestPerimerter(t *testing.T) {
 
 			if result != tc.expected {
 				t.Errorf("Perimeter(%.2f, %.2f) = %.2f; want %.2f", tc.a, tc.b, result, tc.expected)
+			}
+		})
+	}
+}
+
+func TestArea(t *testing.T) {
+	testCases := []struct {
+		name     string
+		a        float64
+		b        float64
+		expected float64
+	}{
+		{
+			name:     "calculating a positive Area",
+			a:        12.0,
+			b:        6.0,
+			expected: 72.0,
+		},
+		{
+			name:     "calculating a zero value Area",
+			a:        0.0,
+			b:        0.0,
+			expected: 0.0,
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			result := Area(tc.a, tc.b)
+
+			if result != tc.expected {
+				t.Errorf("Area(%.2f, %.2f) = %.2f; want %.2f", tc.a, tc.b, result, tc.expected)
 			}
 		})
 	}
