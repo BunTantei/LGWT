98614d708f183780eed14161b6323be05d20b155
bun <vincent.tantei@proton.me>
2025-11-15 08:31:59 +0100

    learning about methods and struct and interface and how to use them and how to setup correctly and nicely a TDT

--- structs-methods-interfaces/smi.go
+++ structs-methods-interfaces/smi.go
@@ -6,10 +6,35 @@ type Rectangle struct {
 	Height float64
 }
 
+type Circle struct {
+	Radius float64
+}
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+// Shape interface - any type with an Area() method is a Shape
+type Shape interface {
+	Area() float64
+}
+
 func Perimeter(rec Rectangle) float64 {
 	return 2 * (rec.Width + rec.Height)
 }
 
-func Area(rec Rectangle) float64 {
-	return rec.Width * rec.Height
+// Area method on Rectangle
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+// Area method on Circle
+func (c Circle) Area() float64 {
+	return 3.141592653589793 * c.Radius * c.Radius
+}
+
+// Area method on Triangle
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
 }
--- structs-methods-interfaces/smi_test.go
+++ structs-methods-interfaces/smi_test.go
@@ -1,6 +1,8 @@
 package structsmethodsinterfaces
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestPerimeter(t *testing.T) {
 	testCases := []struct {
@@ -37,35 +39,26 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	testCases := []struct {
-		name      string
-		rectangle Rectangle
-		expected  float64
-	}{
-		{
-			name: "calculating a positive Area",
-			rectangle: Rectangle{
-				Width:  12.0,
-				Height: 6.0,
-			},
-			expected: 72.0,
-		},
-		{
-			name: "calculating a zero value Area",
-			rectangle: Rectangle{
-				Width:  0.0,
-				Height: 0.0,
-			},
-			expected: 0.0,
-		},
-	}
-	for _, tc := range testCases {
-		t.Run(tc.name, func(t *testing.T) {
-			result := Area(tc.rectangle)
 
-			if result != tc.expected {
-				t.Errorf("Area(%g) = %g; want %g", tc.rectangle, result, tc.expected)
+	areaTests := []struct {
+		name    string
+		shape   Shape
+		hasArea float64
+	}{
+		{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, hasArea: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
+	}
+
+	for _, tt := range areaTests {
+		// using tt.name from the case to use it as the `t.Run` test name
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
 			}
 		})
+
 	}
+
 }
