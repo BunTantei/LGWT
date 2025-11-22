09e0e26e778ee4586a9d369f7f668deda797bcc8
bun <vincent.tantei@proton.me>
2025-11-22 11:59:35 +0100

    making similar to the syntax i used before to get used to one way

--- structs-methods-interfaces/smi_test.go
+++ structs-methods-interfaces/smi_test.go
@@ -40,22 +40,22 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 
-	areaTests := []struct {
-		name    string
-		shape   Shape
-		hasArea float64
+	testCases := []struct {
+		name     string
+		shape    Shape
+		expected float64
 	}{
-		{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, hasArea: 72.0},
-		{name: "Circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
-		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
+		{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, expected: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10}, expected: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, expected: 36.0},
 	}
 
-	for _, tt := range areaTests {
+	for _, tc := range testCases {
 		// using tt.name from the case to use it as the `t.Run` test name
-		t.Run(tt.name, func(t *testing.T) {
-			got := tt.shape.Area()
-			if got != tt.hasArea {
-				t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
+		t.Run(tc.name, func(t *testing.T) {
+			got := tc.shape.Area()
+			if got != tc.expected {
+				t.Errorf("%#v got %g want %g", tc.shape, got, tc.expected)
 			}
 		})
 
