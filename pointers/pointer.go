7cd83156366edc6e1d22933c8393d27fa46ac97b
bun <vincent.tantei@proton.me>
2025-12-01 12:59:06 +0100

    starting to work with pointers

--- pointers/pointer.go
+++ pointers/pointer.go
@@ -1,2 +1,14 @@
 // Package pointers is everything to learn about pointers but also errors.
 package pointers
+
+type Wallet struct {
+	balance int
+}
+
+func (w Wallet) Deposit(amount int) {
+
+}
+
+func (w Wallet) Balance() int {
+	return w.balance
+}
