228ac9830c0c6c573d0692e0393762a6a1262494
bun <vincent.tantei@proton.me>
2025-12-02 14:10:33 +0100

    continue working on pointers

--- pointers/pointer.go
+++ pointers/pointer.go
@@ -1,7 +1,10 @@
 // Package pointers is everything to learn about pointers but also errors.
 package pointers
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 type Bitcoin int
 
@@ -17,13 +20,24 @@ type Wallet struct {
 	balance Bitcoin
 }
 
+var ErrInsufficientFunds = errors.New("Cannot withdraw, insufficient funds.")
+
 func (w *Wallet) Deposit(amount Bitcoin) {
+	fmt.Printf("The amount deposit is %d \n", amount)
 	fmt.Printf("address of balance in Deposit is %p \n", &w.balance)
 	w.balance += amount
+	fmt.Printf("The balance after deposit is %d \n", w.balance)
 }
 
-func (w *Wallet) Withdraw(amount Bitcoin) {
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+
+	if amount > w.balance {
+		return ErrInsufficientFunds
+	}
+	fmt.Printf("amount withdraw is %d \n", amount)
 	w.balance -= amount
+	fmt.Printf("the balance after withdrawing is %d \n", w.balance)
+	return nil
 }
 
 func (w *Wallet) Balance() Bitcoin {
