075d13174b4c17c7af9e6c4f9a773d78852289ea
bun <vincent.tantei@proton.me>
2025-12-02 04:55:40 +0100

    adding the withdraw method

--- pointers/pointer.go
+++ pointers/pointer.go
@@ -22,6 +22,9 @@ func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
+func (w *Wallet) Withdraw(amount Bitcoin) {
+	w.balance -= amount
+}
 func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
